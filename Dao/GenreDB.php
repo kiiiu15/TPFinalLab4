6185670b7d62381d48074f26131ea7a8fb356196
kiiiu15 <44479717+kiiiu15@users.noreply.github.com>
2019-10-30 15:39:09 -0300

    New PDO Genre PDO Movie

---
+++ Dao/GenreDB.php
@@ -0,0 +1,109 @@
+<?php
+namespace Dao;
+use \PDO as PDO;
+use \Exception as Exception;
+use Dao\QueryType as QueryType;
+use model\Genre as Genre;
+
+class GenreDB {
+
+    private $connection;
+
+    public function __construct(){
+        
+    }
+
+    public function GetAll(){
+        $sql="SELECT * FROM Genres";
+        try{
+            $this->connection= Connection::getInstance();
+            $result = $this->connection->Execute($sql);
+        }catch(\PDOException $ex){
+            throw $ex;
+        }
+        if(!empty($result)){
+            return $this->Map($result);            
+        }else{
+             return false;
+        }
+    }
+
+    public function extractGenrebyId($Id){
+        $sql="SELECT *FROM Genres WHERE Genres.idGenre = :idGenre";
+        $values['idGenre'] = $Id;
+        try{
+            $this->connection= Connection::getInstance();
+            $result = $this->connection->Execute($sql, $values);
+        }catch(\PDOException $ex){
+            throw $ex;
+        }
+
+        if(!empty($result)){
+            return $this->Map($result);            
+        }else{
+             return false;
+        }
+    }
+
+    public function GenreExist($nameToSearch){
+        $sql="SELECT  IFNULL(COUNT(Genres.nameGenre), 0 ) as Cantidad FROM Genres WHERE Genres.nameGenre = :nameGenre GROUP BY Genres.nameGenre";
+        $values['nameGenre'] = $nameToSearch;
+        try{
+            $this->connection= Connection::getInstance();
+            $result = $this->connection->Execute($sql, $values);
+        }catch(\PDOException $ex){
+            throw $ex;
+        }
+
+        
+
+        if(isset($result) && !empty($result) && $result[0]["Cantidad"] > 0){
+            return true;            
+        }else{
+             return false;
+        }
+    }
+
+    public function Add($genre){
+        $sql="INSERT INTO Genres (idGenre , nameGenre) VALUES (:idGenre , :nameGenre)";
+        $values['idGenre'] = $genre->getId();
+        $values['nameGenre'] = $genre->getName();
+
+        try{
+            $this->connection = Connection::getInstance();
+            $this->connection->connect();
+            $result=$this->connection->ExecuteNonQuery($sql,$values);
+            return $result;
+        }catch(\PDOException $ex){
+            throw $ex;
+        }
+    }
+
+    public function Remove($id){
+        $sql="DELETE FROM Genres WHERE Genres.idGenre = :idGenre";
+        $values['idGenre'] = $id;
+
+        try{
+            $this->connection= Connection::getInstance();
+            $this->connection->connect();
+            $result=$this->connection->ExecuteNonQuery($sql,$values);
+            return $result;
+        }catch(\PDOException $ex){
+            throw $ex;
+        }
+       
+    }
+
+
+
+
+    protected function Map($value) {
+        $value = is_array($value) ? $value : [];
+        $resp = array_map(function ($g) {
+            return new Genre($g['idGenre'], $g['nameGenre']);
+        }, $value);
+        return count($resp) > 1 ? $resp : $resp['0'];
+    }
+
+}
+?>
--- Dao/MovieDB.php
+++ Dao/MovieDB.php
@@ -3,7 +3,9 @@ namespace Dao;
 use \PDO as PDO;
 use \Exception as Exception;
 use Dao\QueryType as QueryType;
-use model\Cinema as Cinema;
+use model\Movie as Movie;
+use model\Genre as Genre;
+use Dao\GenreDB as GenreDB;
 
 class MovieDB{
     private $connection;
@@ -27,22 +29,52 @@ class MovieDB{
     }
     
     //FALTA EL LLAMADO A LA API, DE ESO TE ENCARGAS VOS BULZOMI
-    public function Add($cinema){
-        $sql="INSERT INTO Movies (title,language,overview,releaseDate,poster,genres) VALUES (:tittle,:language,:overview,:releaseDate,:poster,:genres)";
-        $values['title']      = $cinema->getTitle();
-        $values['language']    = $cinema->getLanguage();
-        $values['overview']    = $cinema->getOverview();
-        $values['releaseDate'] = $cinema->getReleaseDate();
-        $values['poster']      = $cinema->getPoster();
-        $values['genres']      = $cinema->getGenres();
+    public function Add($movie){
+        $sql="INSERT INTO Movies (idMovie,tittle,language,overview,releaseDate,poster) VALUES (:idMovie,:tittle,:language,:overview,:releaseDate,:poster)";
+        $values['idMovie']     = $movie->getId(); 
+        $values['tittle']       = $movie->getTitle();
+        $values['language']    = $movie->getLanguage();
+        $values['overview']    = $movie->getOverview();
+        $values['releaseDate'] = $movie->getReleaseDate();
+        $values['poster']      = $movie->getPoster();
+        //$values['genres']      = $movie->getGenres();
+
+        $genrePerMovie = $movie->getGenres();
+
+
+
+
+
+
+        
 
         try{
             $this->connection =Connection::getInstance();
             $this->connection->connect();
             $this->connection->ExecuteNonQuery($sql,$values);
+
+
+            $genreDB = new GenreDB();
+
+
+            foreach ($genrePerMovie as $genreMovie){
+                $sql2= "INSERT INTO GenresPerMovie (idMovie, idGenre) VALUES (:idMovie,:idGenre)";
+                $values2 ['idMovie'] = $movie->getId();
+                $values2 ['idGenre'] = $genreMovie->getId();
+    
+                
+                   /* $this->connection =Connection::getInstance();
+                    $this->connection->connect();*/
+                    $this->connection->ExecuteNonQuery($sql2,$values2);
+
+                $genreDB->Add($genreMovie);
+    
+            }
         }catch(\PDOExeption $ex){
             throw $ex;
         }
+
+
     }
 
     public function RetrieveByGenre($genre){
@@ -92,6 +124,20 @@ class MovieDB{
         }catch(\PDOException $ex){
             throw $ex;
         }
+    }
+
+    protected function Map($value) {
+        $value = is_array($value) ? $value : [];
+        $resp = array_map(function ($m) {
+            
+
+
+
+
+
+
+        }, $value);
+        return count($resp) > 1 ? $resp : $resp['0'];
     }
 
 }
