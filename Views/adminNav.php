111e7cb85fef08eed1c565587e49c9361655f87b
Manuel Sureda <manusuremdq97@gmail.com>
2019-11-07 19:24:05 -0300

    correccion admin nav

--- Views/adminNav.php
+++ Views/adminNav.php
@@ -11,7 +11,7 @@
                 <a class="nav-link" href="<?= FRONT_ROOT ?>">Show List Movies</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="<?= FRONT_ROOT . '/Views/payment' ?>">Payment</a>
+                <a class="nav-link" href="<?= VIEWS . '/payment' ?>">Payment</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?= FRONT_ROOT . '/Cinema' ?>">Add Cinema</a>   
