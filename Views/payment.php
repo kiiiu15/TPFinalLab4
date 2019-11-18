f8dc4f172dc711437276415ed6e8f52680fae853
Manuel Sureda <manusuremdq97@gmail.com>
2019-11-18 20:44:16 -0300

    Update payment.php

--- Views/payment.php
+++ Views/payment.php
@@ -1,3 +1,7 @@
+<?php
+include(VIEWS."/header2.php");
+include(VIEWS.'/nav2.php');
+?>
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
