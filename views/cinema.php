b46d34bfd60c05bb16d5d0fc62e55becebd54cfc
kiiiu15 <tinbulzomi@hotmail.com>
2024-04-21 21:46:55 -0300

    fix responsive

--- views/cinema.php
+++ views/cinema.php
@@ -11,8 +11,8 @@ if (!is_array($cinemaList)) {
 
 ?>
 
-<main class="p-5">
-    <div class="container">
+<main class="p-2 p-md-5">
+    <div class="container-md">
 
         <h1 class="mb-5">List of Cinemas</h1>
 
@@ -21,45 +21,49 @@ if (!is_array($cinemaList)) {
         } ?>
 
         <form action="<?= FRONT_ROOT ?>/Cinema/ChangeCinemaState" method="POST">
-            <div class="bg-light p-3 rounded d-flex justify-content-between">
-                <div class="form-inline">
-                    <div class="form-group ">
-                        <label for="active">Active/Inactive</label>
-                        <select name="active" class="form-control mx-2">
+            <div class="bg-light p-3 rounded d-flex flex-column flex-sm-row justify-content-sm-between">
+                <div class="form-inline flex-column align-items-stretch flex-sm-row ">
+                    <div class="form-group d-flex flex-column flex-sm-row d-sm-inline-flex align-items-sm-baseline">
+                        <label class="mb-0" for="active">Active/Inactive</label>
+                        <select name="active" class="form-control mx-sm-2">
                             <option value="true">Active</option>
                             <option value="false">Inactive</option>
                         </select>
                     </div>
-                    <button type="submit" class="btn btn-dark">Send</button>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-dark btn-block">Send</button>
+                    </div>
                 </div>
                 <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#add-cinema">
                     Add a Cinema
                 </button>
             </div>
 
-            <table class="table table-light table-striped">
-                <thead class="thead-dark">
-                    <tr>
-                        <th></th>
-                        <th>Id Cinema</th>
-                        <th>Name</th>
-                        <th>Adress</th>
-                        <th>State</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($cinemaList as $cinema) { ?>
+            <div class="table-responsive">
+                <table class="table table-light table-striped">
+                    <thead class="thead-dark">
                         <tr>
-                            <td><input type="checkbox" name="postschecked[]" value="<?php echo $cinema->getIdCinema(); ?>"></td>
-                            <td> <?php echo $cinema->getIdCinema(); ?> </td>
-                            <td> <?php echo $cinema->getName(); ?> </td>
-                            <td> <?php echo $cinema->getAddress(); ?> </td>
-                            <td> <?php echo ($cinema->getActive()) ? "Active" : "Inactive"; ?></td>
-
+                            <th></th>
+                            <th>Id Cinema</th>
+                            <th>Name</th>
+                            <th>Adress</th>
+                            <th>State</th>
                         </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($cinemaList as $cinema) { ?>
+                            <tr>
+                                <td><input type="checkbox" name="postschecked[]" value="<?php echo $cinema->getIdCinema(); ?>"></td>
+                                <td> <?php echo $cinema->getIdCinema(); ?> </td>
+                                <td> <?php echo $cinema->getName(); ?> </td>
+                                <td> <?php echo $cinema->getAddress(); ?> </td>
+                                <td> <?php echo ($cinema->getActive()) ? "Active" : "Inactive"; ?></td>
+
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </form>
     </div>
 </main>
