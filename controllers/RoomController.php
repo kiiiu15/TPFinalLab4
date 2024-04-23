c10dc328749f20b00a9a1f6e72191199c500a66d
kiiiu15 <tinbulzomi@hotmail.com>
2024-04-23 15:24:39 -0300

    rename rooms file

--- controllers/RoomController.php
+++ controllers/RoomController.php
@@ -151,7 +151,7 @@ class RoomController implements IControllers{
         $activeCinemas=$this->TransformToArray($CinemaList);
         
         
-        include(VIEWS . "/listRooms.php");
+        include(VIEWS . "/rooms.php");
     }
 
     
