b7b825a079081f2c5f054ea3c12da08c06cebe9e
kiiiu15 <tinbulzomi@hotmail.com>
2024-04-25 17:21:30 -0300

    center modal

--- views/components/purchase/modal.php
+++ views/components/purchase/modal.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="qrModal" tabindex="-1" role="dialog" aria-labelledby="qrModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <p class="h5 modal-title" id="qrModalLabel">Your QR</p>
