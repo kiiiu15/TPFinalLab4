84435f7e70bd87857e70b27b69ae6ee44a1f583b
kiiiu15 <tinbulzomi@hotmail.com>
2024-05-27 23:18:07 -0300

    Refactor MovieController to use private attributes and remove unused imports.

--- controllers/MovieController.php
+++ controllers/MovieController.php
@@ -2,30 +2,31 @@
 
 namespace Controllers;
 
-use \PDO as PDO;
 use \Exception as Exception;
 use Controllers\IControllers as IControllers;
-use Controllers\GenreController as GenreController;
 use model\Movie as Movie;
 use Dao\MovieDB as MovieDB;
 
-
 class MovieController implements IControllers
 {
+    private $movieDB;
+    private $genreController;
+
+    public function __construct()
+    {
+        $this->movieDB = new MovieDB();
+        $this->genreController = new GenreController();
+    }
 
     public function add($title = "", $Language = "", $overview = "", $ReleaseDate = "", $Poster, $genre_ids)
     {
-
-        $MovieDB = new MovieDB();
         try {
             $movie = new Movie($title, $Language, $overview, $ReleaseDate, $Poster, $genre_ids);
-            $MovieDB->Add($movie);
+            $this->movieDB->Add($movie);
         } catch (\PDOException $ex) {
             throw $ex;
         }
     }
-
-
 
     //AUN NO VERIFICADAS !!!!
 
@@ -37,22 +38,18 @@ class MovieController implements IControllers
             $apiContent = array();
         }
 
-        $movieDB = new MovieDB();
         $newMovies = array();
         try {
             if ($apiContent) {
-
                 $arrayToDecode  = json_decode($apiContent, true);
 
-
                 foreach ($arrayToDecode["results"] as $movie) {
-
                     if (!$this->ExistMovie($movie["id"])) //Falta crear la validacion
                     {
                         //Falta la obtencion de generos, creo que esta mal la ultima parte ! 
-                        $movie = new Movie($movie["id"], $movie["title"], $movie["original_language"], $movie["overview"],  $movie["release_date"], $movie["poster_path"], $movieDB->GetObjectGenresForMovie($movie['genre_ids']));
+                        $movie = new Movie($movie["id"], $movie["title"], $movie["original_language"], $movie["overview"],  $movie["release_date"], $movie["poster_path"], $this->movieDB->GetObjectGenresForMovie($movie['genre_ids']));
 
-                        $movieDB->Add($movie);
+                        $this->movieDB->Add($movie);
 
                         $newMovies[$movie->getId()] = $movie;
                     }
@@ -66,9 +63,8 @@ class MovieController implements IControllers
 
     public function ExistMovie($idMovie)
     {
-        $movieDB = new MovieDB();
         try {
-            if ($movieDB->RetrieveById($idMovie) == false) {
+            if ($this->movieDB->RetrieveById($idMovie) == false) {
                 return false;
             } else {
                 return true;
@@ -83,8 +79,7 @@ class MovieController implements IControllers
     public function GetAll()
     {
         try {
-            $MovieDB = new MovieDB();
-            $MovieList = $MovieDB->GetAll();
+            $MovieList = $this->movieDB->GetAll();
             return $MovieList;
         } catch (\PDOException $ex) {
             return array();
@@ -94,9 +89,8 @@ class MovieController implements IControllers
     //Verificar si funca 
     public function GetMovieForGenre($idGenreToSearch)
     {
-        $MovieDB = new MovieDB();
         try {
-            $ListMovieGenre = $MovieDB->RetrieveByGenre($idGenreToSearch);
+            $ListMovieGenre = $this->movieDB->RetrieveByGenre($idGenreToSearch);
             return $ListMovieGenre;
         } catch (\PDOException $ex) {
             return array();
@@ -105,9 +99,8 @@ class MovieController implements IControllers
 
     public function GetMovieForId($id)
     {
-        $MovieDB = new MovieDB();
         try {
-            $movie = $MovieDB->RetrieveById($id);
+            $movie = $this->movieDB->RetrieveById($id);
             return $movie;
         } catch (\PDOException $ex) {
             return null;
@@ -116,9 +109,8 @@ class MovieController implements IControllers
 
     public function GetMovieForTitle($titleMovie)
     {
-        $movieDB = new MovieDB();
         try {
-            $movie = $movieDB->RetrieveByTitle($titleMovie);
+            $movie = $this->movieDB->RetrieveByTitle($titleMovie);
             return $movie;
         } catch (\PDOException $ex) {
             return null;
@@ -128,21 +120,14 @@ class MovieController implements IControllers
     public function index()
     {
         $movieList = $this->GetAll();
-        $genreController = new GenreController();
-        $genresList = $genreController->GetAll();
+        $genresList = $this->genreController->GetAll();
         include(PAGES . '/posts.php');
     }
 
-
     public function prueba($param)
     {
-
-
         $a = $this->GetMovieForId($param);
-
-        $json =  json_encode($a, JSON_PRETTY_PRINT);
-
-
+        $json = json_encode($a, JSON_PRETTY_PRINT);
         echo $json;
     }
 }
