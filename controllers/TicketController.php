882abbc1d982533aa4b1adfb2da693373aa4ba11
kiiiu15 <tinbulzomi@hotmail.com>
2024-05-27 23:22:31 -0300

    Refactor TicketController to use private attributes and remove unused imports.

--- controllers/TicketController.php
+++ controllers/TicketController.php
@@ -1,91 +1,79 @@
-<?php namespace controllers;
+<?php
+
+namespace controllers;
 
 use phpqrcode\QRcode as QRcode;
-use \PDO as PDO;
-use \Exception as Exception;
 use controllers\Icontrollers as Icontrollers;
-use controllers\MovieFunctionController as MovieFunctionController;
-use controllers\UserController as UserController;
-use controllers\BuyController as BuyController;
-
-use model\Buy as Buy;
-use model\MovieFunction as MovieFunction;
-use model\User as User;
-use model\Ticket as Ticket;
-
-use Dao\BuyDB as BuyDB;
-use Dao\UserDB as UserDB;
 use Dao\TicketDB as TicketDB;
 
-class TicketController implements Icontrollers{
+class TicketController implements Icontrollers
+{
+    private $ticketDB;
 
+    public function __construct()
+    {
+        $this->ticketDB = new TicketDB();
+    }
 
-    public function Add($ticket){
-        $ticketDB = new TicketDB();
-
-        try{
-            $ticketDB->Add($ticket);
-        }catch(\PDOException $ex){
+    public function Add($ticket)
+    {
+        try {
+            $this->ticketDB->Add($ticket);
+        } catch (\PDOException $ex) {
             echo "Error connection";
         }
     }
 
-    public function GetAll(){
-        $ticketDB = new TicketDB();
-        $ticketList = array();
-
-        try{
-            $ticketList = $ticketDB->GetAll();
+    public function GetAll()
+    {
+        try {
+            $ticketList = $this->ticketDB->GetAll();
             return $ticketList;
-        }catch(\PDOException $ex){
+        } catch (\PDOException $ex) {
             return array();
         }
-        
     }
 
-    public function Delete($ticket){
-        $ticketDB = new TicketDB();
-        try{
-            $ticketDB->Delete($ticket);
-        }catch(\PDOException $ex){
-            echo "Error Coneection";
-        }
-    }
-
-    public function RetrieveById($idTicket){
-        $ticketDB = new TicketDB();
-        $ticket = new Ticket();
-        try{
-            $ticket = $ticketDB->RetrieveById($idTicket);
-            return $ticket;
-        }catch(\PDOException $ex){
-            return null;
-        }
-        
-    }
-
-    public function RetrieveByIdBuy($idBuy){
-        $ticketDB = new TicketDB();
-        $ticket = new Ticket();
-        try{
-            $ticket = $ticketDB->RetrieveByIdBuy($idBuy);
-            return $ticket;
-        }catch(\PDOException $ex){
-            return null;
-        }
-        
-    }
-
-    public function RetrieveByUser($user){
-        $ticketDB = new TicketDB();
+    public function Delete($ticket)
+    {
         try {
-            return $ticketDB->RetrieveByUser($user);
+            $this->ticketDB->Delete($ticket);
+        } catch (\PDOException $ex) {
+            echo "Error Connection";
+        }
+    }
+
+    public function RetrieveById($idTicket)
+    {
+        try {
+            $ticket = $this->ticketDB->RetrieveById($idTicket);
+            return $ticket;
+        } catch (\PDOException $ex) {
+            return null;
+        }
+    }
+
+    public function RetrieveByIdBuy($idBuy)
+    {
+        try {
+            $ticket = $this->ticketDB->RetrieveByIdBuy($idBuy);
+            return $ticket;
+        } catch (\PDOException $ex) {
+            return null;
+        }
+    }
+
+    public function RetrieveByUser($user)
+    {
+        try {
+            return $this->ticketDB->RetrieveByUser($user);
         } catch (\Throwable $th) {
             return array();
         }
     }
 
-    public function GenerateRandomString($length = 4) {
+    public function GenerateRandomString($length = 4)
+    {
         $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $charactersLength = strlen($characters);
         $randomString = '';
@@ -93,36 +81,25 @@ class TicketController implements Icontrollers{
             $randomString .= $characters[rand(0, $charactersLength - 1)];
         }
         return $randomString;
-    } 
+    }
 
-    public function GenerateQR($randomString){
+    public function GenerateQR($randomString)
+    {
         $dir = 'QRS/';
-        if(!file_exists($dir)){
+        if (!file_exists($dir)) {
             mkdir($dir);
-        }else{
-            $filename = $dir .$randomString .'.png';
+        } else {
+            $filename = $dir . $randomString . '.png';
             $tamanio = 10;
             $level = 'M';
             $frameSize = 3;
             $content = $randomString;
 
-            QRcode::png($content, $filename, $level , $tamanio , $frameSize);
+            QRcode::png($content, $filename, $level, $tamanio, $frameSize);
         }
     }
 
-  
-
-
-    public function index(){}
+    public function index()
+    {
+    }
 }
-
-
-
-
-
-
-
-
-
-
-?>
