1554d03018022c4415d36a0800387fbb7fb651ae
kiiiu15 <tinbulzomi@hotmail.com>
2024-05-27 22:51:14 -0300

    move db to class property

--- controllers/GenreController.php
+++ controllers/GenreController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Controllers;
+
 use \PDO as PDO;
 use \Exception as Exception;
 use Controllers\IControllers as IControllers;
@@ -7,52 +9,53 @@ use model\Genre as Genre;
 use Dao\GenreDB as GenreDB;
 
 
-class GenreController implements IControllers{
+class GenreController implements IControllers
+{
 
-    public function add ($id = 0,$name = "") {
-        
-        $genreDB= new GenreDB();
-        $genre= new Genre($id,$name);
-        try{
-            $genreDB->Add($genre);
-        }catch(\PDOException $ex){
-            echo "Error ading genre";
-        }
+    private GenreDB $genreDB;
 
-        
+    public function __construct() {
+        $this->genreDB = new GenreDB();
     }
 
-    public function GetAll(){
-        $genreDB= new GenreDB();
-        try{
-            $genreList= $genreDB->GetAll();
-        }catch(\PDOException $ex){
+
+    public function add($id = 0, $name = "")
+    {
+        try {
+            $genre = new Genre($id, $name);
+            $this->genreDB->Add($genre);
+        } catch (\PDOException $ex) {
+            echo "Error ading genre";
+        }
+    }
+
+    public function GetAll()
+    {
+        try {
+            $genreList = $this->genreDB->GetAll();
+        } catch (\PDOException $ex) {
             $genreList = array();
         }
         return $genreList;
     }
 
-    public function index(){
-       
+    public function index()
+    {
     }
 
-    public function RetrieveAPI(){
-        $genreDB = new GenreDB ();
+    public function RetrieveAPI()
+    {
         $apiContent = file_get_contents("https://api.themoviedb.org/3/genre/movie/list?api_key=f78530630a382b20d19bddc505aac95d&language=en-US");
-        try{
-            if ($apiContent){
-                $arrayToDecode  = json_decode($apiContent,true); 
-                foreach ($arrayToDecode['genres'] as $genre){
-                    $newGenre = new Genre($genre["id"],$genre["name"]);
-                    $genreDB->Add($newGenre);
+        try {
+            if ($apiContent) {
+                $arrayToDecode  = json_decode($apiContent, true);
+                foreach ($arrayToDecode['genres'] as $genre) {
+                    $newGenre = new Genre($genre["id"], $genre["name"]);
+                    $this->genreDB->Add($newGenre);
                 }
             }
-        }catch(\PDOException $ex){
+        } catch (\PDOException $ex) {
             echo "DB connection Error";
         }
     }
-
 }
-
-
-?>
